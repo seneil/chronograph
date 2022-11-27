307cbebd49b1f5626d44f1ddf4442f32f6b7b824
d.shvalyov <d.shvalyov@fun-box.ru>
2022-11-27 13:12:20 +0300

    Установка библиотеки UI BlueprintJS

--- frontend/chronograph.tsx
+++ frontend/chronograph.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { FocusStyleManager, Button } from "@blueprintjs/core";
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
@@ -9,8 +11,19 @@ interface ApplicationProps {
   children?: React.ReactNode;
 }
 
+FocusStyleManager.onlyShowFocusOnTabs();
+
 const Chronograph: React.FC<ApplicationProps> = ({ title }) => (
-  <h2>{title}</h2>
+  <>
+    <h2>{title}</h2>
+    <Button
+      icon="refresh"
+      text="Sample button"
+      onClick={event => {
+        console.log(event)
+      }}
+    />
+  </>
 );
 
 root.render(<Chronograph title="Hello from React 18"/>);
