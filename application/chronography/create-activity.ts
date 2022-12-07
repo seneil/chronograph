0a514bb88cb4117d66793186cdc22b24c66cd31a
dshster <dsh-creative@yandex.ru>
2022-12-07 22:35:50 +0300

    Разработка функции парсинга строки активности

---
+++ application/chronography/create-activity.ts
@@ -0,0 +1,35 @@
+import { Activity } from '../types';
+
+import { parseTimeString } from './utils/parse-time-string';
+
+export const getActivity = (friendlyString: string): Activity => {
+  const result: Activity = {
+    activity: friendlyString,
+    category: null,
+    endTime: null,
+    startTime: null,
+  };
+
+  const parts: string[] = friendlyString.split(' ');
+  const [timeString]: string[] = parts;
+
+  const timeStringRegexp = /^[1-9|\-|:].*$/ig;
+
+  if (timeStringRegexp.test(timeString)) {
+    const isTimeRange = timeString.indexOf('-') > 0;
+
+    result.startTime = isTimeRange ? parseTimeString(timeString.substring(0, timeString.indexOf('-'))) : parseTimeString(timeString);
+    result.endTime = isTimeRange ? parseTimeString(timeString.substring(timeString.indexOf('-') + 1)) : result.endTime;
+
+    parts.shift();
+  } else {
+    result.startTime = new Date();
+  }
+
+  const [activity, category = result.category]: string[] = parts.join(' ').split('@');
+
+  result.activity = activity;
+  result.category = category;
+
+  return result;
+};
---
+++ application/chronography/tests/parse-time-string.test.ts
@@ -0,0 +1,34 @@
+import { parseTimeString } from '../utils/parse-time-string';
+
+const toLocal = (now: Date) => now.toString();
+
+describe('Проверка парсинга времени', () => {
+  it('Отправка пустой строки должно вернуть null', () => {
+    expect(parseTimeString('')).toBeNull();
+  });
+
+  it('Время 10:18 должно вернуть дату с установленным временем 10:18', () => {
+    const now: Date = new Date();
+
+    now.setHours(10);
+    now.setMinutes(18);
+
+    expect(toLocal(parseTimeString('10:18'))).toBe(toLocal(now));
+  });
+
+  it('Отрицательное значение должно вернуть время за вычетом количества переданных минут', () => {
+    const now: Date = new Date();
+
+    expect(toLocal(parseTimeString('-10'))).toBe(toLocal(
+      new Date(now.getTime() - 10 * 1000 * 60)
+    ));
+  });
+
+  it('Отрицательное значение больше часа должно вернуть время за вычетом количества переданных минут', () => {
+    const now: Date = new Date();
+
+    expect(toLocal(parseTimeString('-120'))).toBe(toLocal(
+      new Date(now.getTime() - 120 * 1000 * 60)
+    ));
+  });
+});
