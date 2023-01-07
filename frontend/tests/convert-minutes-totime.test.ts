a7dbbf9f92a07fa8258d2ae7b49ea0be4c7a0be1
dshster <dsh-creative@yandex.ru>
2023-01-07 23:21:36 +0300

    Изменение локализации при форматировании минут в строку времени

--- frontend/tests/convert-minutes-totime.test.ts
+++ frontend/tests/convert-minutes-totime.test.ts
@@ -1,19 +1,23 @@
 import { convertMinutesToTime } from '@frontend/utils/convert-minutes-totime';
 
 describe('Проверка конвертации минут в строку времени', () => {
-  it('Должно быть возвращено 1h', () => {
-    expect(convertMinutesToTime(60)).toBe('1h');
+  it('Должна быть возвращена пустая строка', () => {
+    expect(convertMinutesToTime(0)).toBe('');
   });
 
-  it('Должно быть возвращено 30m', () => {
-    expect(convertMinutesToTime(30)).toBe('30m');
+  it('Должно быть возвращено 1ч', () => {
+    expect(convertMinutesToTime(60)).toBe('1ч');
   });
 
-  it('Должно быть возвращено 1h 30m', () => {
-    expect(convertMinutesToTime(90)).toBe('1h 30m');
+  it('Должно быть возвращено 30м', () => {
+    expect(convertMinutesToTime(30)).toBe('30м');
   });
 
-  it('Должно быть возвращено 3h 45m', () => {
-    expect(convertMinutesToTime(225)).toBe('3h 45m');
+  it('Должно быть возвращено 1ч 30м', () => {
+    expect(convertMinutesToTime(90)).toBe('1ч 30м');
+  });
+
+  it('Должно быть возвращено 3ч 45м', () => {
+    expect(convertMinutesToTime(225)).toBe('3ч 45м');
   });
 });
