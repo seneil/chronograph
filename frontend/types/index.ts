57e867fb7752089b5030b0bfecf94547c09db030
dshster <dsh-creative@yandex.ru>
2023-02-05 18:18:56 +0300

    Разработка суммарного отчёта по категориям за день

--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -2,8 +2,14 @@ import { ActivityCalendarView } from '@application/types/views/activity';
 
 export { ElectronGlobalService, ElectronGlobalFetcher } from './globals';
 
+export interface CategorySummary {
+  categoryName: string;
+  total: number;
+}
+
 export interface ActivityCalendar {
   date: string;
   total: number;
+  summary: CategorySummary[];
   activities: ActivityCalendarView[];
 }
