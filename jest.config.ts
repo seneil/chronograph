36e0493ee013b00b04c223cede31ec7d32c3a6e9
dshster <dsh-creative@yandex.ru>
2023-02-07 21:53:46 +0300

    Исправление маски поиска файлов тестов

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
   testEnvironment: 'node',
   verbose: true,
   transformIgnorePatterns: ['node_modules/(?!variables/.*)'],
-  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: ['**/tests/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: {
     '^@application(.*)$': '<rootDir>/application/$1',
     '^@frontend(.*)$': '<rootDir>/frontend/$1',
